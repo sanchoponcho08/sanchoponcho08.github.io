2e582c6b74765464fd69e815354c502a48eee105
sanchoponcho08 <kevinsito1559@gmail.com>
2024-10-20 13:18:09 -0300

    primeravista casi terminada + nav con css y javascript terminado

--- principal.js
+++ principal.js
@@ -1,22 +1,37 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const nav = document.querySelector("nav"); // Seleccionar el nav
+
+  // Agregar el evento de scroll
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+      // Si el scroll es mayor que 50px
+      nav.classList.add("scrolled"); // Añade la clase 'scrolled'
+    } else {
+      nav.classList.remove("scrolled"); // Remueve la clase 'scrolled'
+    }
+  });
+});
+
 let indiceImagen = 0;
-const imagenes = document.querySelectorAll('.imagenes img');
+const imagenes = document.querySelectorAll(".imagenes img");
 
 function mostrarImagen(indice) {
-    if (indice < 0) {
-        indiceImagen = imagenes.length - 1; 
-    } else if (indice >= imagenes.length) {
-        indiceImagen = 0;
-    } else {
-        indiceImagen = indice; 
-    }
+  if (indice < 0) {
+    indiceImagen = imagenes.length - 1;
+  } else if (indice >= imagenes.length) {
+    indiceImagen = 0;
+  } else {
+    indiceImagen = indice;
+  }
 
-    const desplazamiento = -indiceImagen * 100; 
-    document.querySelector('.imagenes').style.transform = `translateX(${desplazamiento}%)`;
+  const desplazamiento = -indiceImagen * 100;
+  document.querySelector(
+    ".imagenes"
+  ).style.transform = `translateX(${desplazamiento}%)`;
 }
 
 function cambiarImagen(direccion) {
-    mostrarImagen(indiceImagen + direccion);
+  mostrarImagen(indiceImagen + direccion);
 }
 
 mostrarImagen(indiceImagen);
-
