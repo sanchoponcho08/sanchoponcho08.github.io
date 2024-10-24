228fe0545390f022b4c9a974d15610e44cbfcf38
sanchoponcho08 <kevinsito1559@gmail.com>
2024-10-24 13:01:39 -0300

    revierto el ultimo commit

--- principal.js
+++ principal.js
@@ -10,27 +10,3 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
-
-let indiceImagen = 0;
-const imagenes = document.querySelectorAll(".imagenes img");
-
-function mostrarImagen(indice) {
-  if (indice < 0) {
-    indiceImagen = imagenes.length - 1;
-  } else if (indice >= imagenes.length) {
-    indiceImagen = 0;
-  } else {
-    indiceImagen = indice;
-  }
-
-  const desplazamiento = -indiceImagen * 100;
-  document.querySelector(
-    ".imagenes"
-  ).style.transform = `translateX(${desplazamiento}%)`;
-}
-
-function cambiarImagen(direccion) {
-  mostrarImagen(indiceImagen + direccion);
-}
-
-mostrarImagen(indiceImagen);
