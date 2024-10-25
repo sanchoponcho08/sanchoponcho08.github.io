ba1724227c92ba926423cecf05a53c951bbbadf5
sanchoponcho08 <kevinsito1559@gmail.com>
2024-10-25 02:23:21 -0300

    mas cambios categoriafem

--- categorias.js
+++ categorias.js
@@ -5,34 +5,34 @@ const imgs = document.querySelectorAll("#categorias .slider-box img");
 const totalImages = imgs.length;
 
 function showImages() {
-    // Desplaza el slider según el índice actual
-    const offset = currentIndex * (100 / imagesToShow);
-    sliderBox.style.transform = `translateX(-${offset}%)`; // Desplazamiento para mostrar imágenes
+  // Desplaza el slider según el índice actual
+  const offset = currentIndex * (100 / imagesToShow);
+  sliderBox.style.transform = `translateX(-${offset}%)`; // Desplazamiento para mostrar imágenes
 }
 
 function nextImage() {
-    currentIndex += 1;
-    if (currentIndex > totalImages - imagesToShow) {
-        currentIndex = totalImages - imagesToShow; // Limita al último grupo
-    }
-    showImages();
+  currentIndex += 1;
+  if (currentIndex > totalImages - imagesToShow) {
+    currentIndex = 0; // Limita al último grupo
+  }
+  showImages();
 }
 
 function prevImage() {
-    currentIndex -= 1;
-    if (currentIndex < 0) {
-        currentIndex = 0; // Limita al primer grupo
-    }
-    showImages();
+  currentIndex -= 1;
+  if (currentIndex < 0) {
+    currentIndex = totalImages - imagesToShow; // Limita al primer grupo
+  }
+  showImages();
 }
 
 // Inicializa el slider
 document.querySelector(".arrow.left").addEventListener("click", () => {
-    prevImage();
+  prevImage();
 });
 
 document.querySelector(".arrow.right").addEventListener("click", () => {
-    nextImage();
+  nextImage();
 });
 
 // Mostrar las imágenes iniciales
